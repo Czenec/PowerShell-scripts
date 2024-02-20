d2a8ee99f3266b3f30090ec743a40c650ad0d916
Czenec <czenec110@gmail.com>
2024-02-20 15:37:58 +0100

    Quick update to this script
    Quick update to this script now that I'm going to use it in NewArubaConfig.ps1
    I don't know why I left that piece of code there, it's just redundant.

--- From school/Ask user with default.ps1
+++ From school/Ask user with default.ps1
@@ -1,6 +1,5 @@
 $default = "default value" # a normal number is writen without parentheses
-$userInput = Read-Host "Enter a value or text (Default: default value)"
-If(!$userInput) { $userInput = $default }
+$userInput = $(Write-Host "Enter a value or text (Default: default value)  " -ForegroundColor Blue -NoNewline; Read-Host)
 if ([string]::IsNullOrWhiteSpace($userInput)) {
     $userInput = $default
 }
